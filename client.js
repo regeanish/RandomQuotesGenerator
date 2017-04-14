1d02fabdf924bc66e54454952b057e5e4465e2f0
regeanish <regeanish@gmail.com>
2017-04-13 22:02:01 -0700

    Second Commit

--- client.js
+++ client.js
@@ -8,29 +8,27 @@ Quote();
 function Quote() { 
  
     
-$.ajax({
-      url: "http://api.forismatic.com/api/1.0/?",
-      dataType: "jsonp",
-      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-      success: function( response ) {
-        $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
-          response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
-        $("#tweet").attr("href", "http://twitter.com/home/?status=" + response.quoteText +
-          ' (' + response.quoteAuthor + ')');
-      }
-  });
-
-var quotes = ["Never answer a critic, unless he's right.", "If you are a dog and your owner suggests that you wear a sweater, suggest that he wear a tail.", "Tradition is a guide and not a jailer."]
-var authors = ["Bernard M. Baruch", "Fran Lebowitz ", "W. Somerset Maugham"];
 
 
-randomNumber = Math.floor((Math.random()*quotes.length));
-  
-randomQuote = quotes[randomNumber];
-randomAuthor = authors[randomNumber];    
+//var quotes = ["Never answer a critic, unless he's right", "If you are a dog and your owner suggests that you wear a sweater, suggest that he wear a tail", "Tradition is a guide and not a jailer"]
+//var authors = ["Bernard M. Baruch", "Fran Lebowitz ", "W. Somerset Maugham"];
+//
+//
+//randomNumber = Math.floor((Math.random()*quotes.length));
+//  
+//randomQuote = quotes[randomNumber];
+//randomAuthor = authors[randomNumber]; 
 
-$("#quote").text(randomQuote);
-$("#author").text(randomAuthor); 
+    
+var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+$.getJSON(url, function(data){
+   $("#quote").html('"'+data.quoteText+'"');
+    $("#author").html('-'+data.quoteAuthor);
+    
+});
+    
+//$("#quote").text(randomQuote);
+//$("#author").text(randomAuthor); 
 
 }
 
@@ -39,9 +37,9 @@ Quote();
     
 });  
     
-$("#tweet").on("click", function(){
-window.open("https://www.twitter.com/intent/tweet?text="+randomQuote + " -" + randomAuthor );
-
-});  
+//$("#tweet").on("click", function(){
+//window.open("https://www.twitter.com/intent/tweet?text="+randomQuote + " -" + randomAuthor );
+//
+//});  
     
 });
