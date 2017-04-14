f86882bec38fef13f33e6180a56824ca7bd08e91
regeanish <regeanish@gmail.com>
2017-04-13 22:21:06 -0700

    First Commit

--- client.js
+++ client.js
@@ -8,8 +8,6 @@ Quote();
 function Quote() { 
  
     
-
-
 //var quotes = ["Never answer a critic, unless he's right", "If you are a dog and your owner suggests that you wear a sweater, suggest that he wear a tail", "Tradition is a guide and not a jailer"]
 //var authors = ["Bernard M. Baruch", "Fran Lebowitz ", "W. Somerset Maugham"];
 //
@@ -18,28 +16,35 @@ function Quote() {
 //  
 //randomQuote = quotes[randomNumber];
 //randomAuthor = authors[randomNumber]; 
+    
+var quoteText, quoteAuthor;
 
     
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+    
 $.getJSON(url, function(data){
+    
    $("#quote").html('"'+data.quoteText+'"');
     $("#author").html('-'+data.quoteAuthor);
+    quoteText = data.quoteText;
+    quoteAuthor = data.quoteAuthor;
+    console.log("quoteText: ", data.quoteText);
+    console.log("quoteAuthor: ", data.quoteAuthor);
     
+    $("#tweet").on("click", function(){
+    window.open("https://www.twitter.com/intent/tweet?text="+ quoteText + "-" + quoteAuthor );   
 });
     
 //$("#quote").text(randomQuote);
 //$("#author").text(randomAuthor); 
-
+ 
+}); 
+    
 }
 
 $("#newQuote").on("click", function(){
 Quote();
-    
+       
 });  
-    
-//$("#tweet").on("click", function(){
-//window.open("https://www.twitter.com/intent/tweet?text="+randomQuote + " -" + randomAuthor );
-//
-//});  
     
 });
