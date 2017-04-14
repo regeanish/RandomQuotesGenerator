1536ac28ed9dc2428930ca427d84caa60b5101b4
regeanish <regeanish@gmail.com>
2017-04-13 20:58:19 -0700

    Second commit

--- client.js
+++ client.js
@@ -1,24 +1,47 @@
 $(document).ready(function(){
-  
-  function Quote() { 
+var randomNumber;
+var randomQuote
+var randomAuthor;
+
+Quote();
+    
+function Quote() { 
  
+    
+$.ajax({
+      url: "http://api.forismatic.com/api/1.0/?",
+      dataType: "jsonp",
+      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+      success: function( response ) {
+        $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
+          response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+        $("#tweet").attr("href", "http://twitter.com/home/?status=" + response.quoteText +
+          ' (' + response.quoteAuthor + ')');
+      }
+  });
+
 var quotes = ["Never answer a critic, unless he's right.", "If you are a dog and your owner suggests that you wear a sweater, suggest that he wear a tail.", "Tradition is a guide and not a jailer."]
 var authors = ["Bernard M. Baruch", "Fran Lebowitz ", "W. Somerset Maugham"];
 
 
-var randomNumber = Math.floor((Math.random()*quotes.length));
+randomNumber = Math.floor((Math.random()*quotes.length));
   
-var RandomQuote = quotes[randomNumber];
-var RandomAuthor = authors[randomNumber];    
+randomQuote = quotes[randomNumber];
+randomAuthor = authors[randomNumber];    
 
-$("#quote").text(RandomQuote);
-$("#author").text(RandomAuthor); 
+$("#quote").text(randomQuote);
+$("#author").text(randomAuthor); 
 
 }
- 
-$(".btn").on("click", function(){
-Quote();
 
-});    
+$("#newQuote").on("click", function(){
+Quote();
+    
+});  
+    
+$("#tweet").on("click", function(){
+window.open("https://www.twitter.com/intent/tweet?text="+randomQuote + " -" + randomAuthor );
+
+});  
     
 });
